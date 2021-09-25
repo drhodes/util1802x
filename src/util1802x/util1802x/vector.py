dcd8a83bdef90d1fc78e09d87511059ba232c742
Derek Rhodes <physci@gmail.com>
2021-09-25 15:13:52 -0400

    cleanup

--- src/util1802x/util1802x/vector.py
+++ src/util1802x/util1802x/vector.py
@@ -2,15 +2,6 @@
 
 import sympy as sp
 
-class Vec(sp.Matrix):
-    pass
-    # '''
-    # A wrapper around sympy.Matrix now
-    # '''
-    # def __init__(self, *args):
-    #     self._matrix = sp.Matrix(args)
-
-    # def __eq__(self, other):
-    #     return self._matrix == other._matrix
+class Vec(sp.Matrix): pass
 
     
